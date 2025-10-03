838cae6126bf24589040d742e682db11a553468c
Zaid Iqbal <ZAIDIQBAL@NIKATBY.COM>
2025-10-03 21:53:24 +0530

    ui and build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Put all node_modules in separate vendor chunk
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1500, // optional: increase limit to 1.5 MB
+  }
+});
